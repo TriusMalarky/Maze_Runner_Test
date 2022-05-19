b30d173bdd02d9f4418c0232c0a42f5de38335b1
TriusMalarky <51863464+TriusMalarky@users.noreply.github.com>
2022-05-18 18:45:54 -0600

    Collision
    Bullets now have collision. This allows one to clip through walls. Will fix,

--- Bullet_Area.gd
+++ Bullet_Area.gd
@@ -1,7 +1,10 @@
-extends Area2D
+extends KinematicBody2D
+
+var velocity = Vector2()
+export (int) var speed = 200
 
 func _init():
 	look_at(get_global_mouse_position())
 
 func _process(delta):
-	position = position * delta
+	velocity = move_and_slide(velocity)
