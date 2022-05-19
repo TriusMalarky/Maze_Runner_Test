9e87a872b798852dfd53221e6c6a7cbbf419017f
TriusMalarky <51863464+TriusMalarky@users.noreply.github.com>
2022-05-18 18:05:42 -0600

    Added Bullet
    Added Bullet Object

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -4,6 +4,8 @@ export (int) var speed = 200
 
 var velocity = Vector2()
 
+export (PackedScene) var Bullet
+
 func get_input():
 	look_at(get_global_mouse_position())
 	velocity = Vector2()
@@ -15,8 +17,15 @@ func get_input():
 		velocity.y += 1
 	if Input.is_action_pressed("up"):
 		velocity.y -= 1
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
+	
+func shoot():
+	var b = Bullet.instance()
+	owner.add_child(b)
+	b.transform = $Muzzle.global_transform
